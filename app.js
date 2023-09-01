efcc3bdf51b7c758c679d5089ee14ef0e3193330
Isaac Aftergut <ijagut@vt.edu>
2023-09-01 17:57:00 -0400

    hello?

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ const nav = document.querySelector('nav')
 const detail = document.querySelector('#detail')
 let players
 const fetchRecipes = async()=>{
-    const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/COHORT-NAME/players')
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
     const json =  await response.json()
-    players = json.data
+    players = json.results
+    console.log(players)
     render()
 }
 const render = ()=>{
     const hash = window.location.hash.slice(1)*1
-    const html = players.players.map((player)=> {
-        return `<a href="#${player.id !== hash ? player.id : ''}" ><div>Player: ${player.name}</div><hr/>
-        <h6>Breed: ${player.breed}</h6></a>`
+    console.log(hash)
+    const html = players.map((player)=> {
+        return `<a href="#${player}" ><div>Pokemon Name: ${player.name}</div>
+        </a>`
     }).join('')
     nav.innerHTML = html
-    const player = players.players.find( player => {
+    const player = players.find( player => {
         return player.id === hash;
     })
     let detailHTML = ''
     if(player){
-        const html2 = `<a class='box extra' href="#"><div>Back to all Puppies</div></a>`
+        const html2 = `<a class='extra' href="#"><div>Back to all Puppies</div></a>`
         nav.innerHTML = html2
         detailHTML =`<div class='box'><div class="name" >Name: ${player.name}</div><hr/><div class="breed" >Breed : ${player.breed}</div><img class='img' src="${player.imageUrl}"/></div>`
     }
